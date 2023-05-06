a3b1f063bfbacf55afce50617eb1b4ceac98fe39
Claire Yuan <claireyu@usc.edu>
2023-05-05 17:25:24 -0700

    cleaned up folder

--- server.py
+++ server.py
@@ -1,104 +1,65 @@
+from flask import Flask, render_template, request
+import pathlib
 import pandas as pd
-import os
 import librosa
 import librosa.display
 import numpy as np
-import IPython.display as ipd
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten
-from tensorflow.keras.optimizers import Adam
-from sklearn import metrics   
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
 
-def features_extractor(file):
-    audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+app = Flask(__name__)
+thisdir = pathlib.Path(
+  __file__).parent.absolute()  # path to directory of this file
+
+app.config['UPLOAD_FOLDER'] = './Uploads'
+
+
+def get_prediction(audio, sample_rate):
+    model = load_model('./model_weights.h5')
+    
     mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-    mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+    mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
     
-    return mfccs_scaled_features
+    mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
     
-   
-# Read csv file
-audio_dataset_path='./Downloads'
-metadata=pd.read_csv('UrbanSound8K.csv')
-metadata.head()
-
-# MFCC algorithm
-mfccs = librosa.feature.mfcc(y=audio_data, sr=sampling_rate, n_mfcc=40)
-
-extracted_features=[]
-for index_num,row in tqdm(metadata.iterrows()):
-    file_name = os.path.join(os.path.abspath(audio_dataset_path),'fold'+str(row["fold"])+'/',str(row["slice_file_name"]))
-    final_class_labels=row["class"]
-    data=features_extractor(file_name)
-    extracted_features.append([data,final_class_labels])
+    # create label encoder instance
+    labelencoder = LabelEncoder()
     
-# Convert list into dataframe
-extracted_features_df=pd.DataFrame(extracted_features,columns=['feature','class'])
-extracted_features_df.head(10)
-
-# Split into input and output
-X=np.array(extracted_features_df['feature'].tolist())
-y=np.array(extracted_features_df['class'].tolist())
-
-labelencoder=LabelEncoder()
-y=to_categorical(labelencoder.fit_transform(y))
-
-# Split into train subset and test subset
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
-
-# --- CREATING THE MODEL ---
-num_labels=y.shape[1]
-
-model=Sequential()
-###first layer
-model.add(Dense(100,input_shape=(40,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-###second layer
-model.add(Dense(200))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-###third layer
-model.add(Dense(100))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-
-###final layer
-model.add(Dense(num_labels))
-model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
-
-num_epochs = 200
-num_batch_size = 32
-
-checkpointer = ModelCheckpoint(filepath='saved_models/audio_classification.hdf5', verbose=1, save_best_only=True)
-                               
-                               
-model.fit(X_train, y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(X_test, y_test), callbacks=[checkpointer], verbose=1)
-
-# Get accuracy of model
-test_accuracy=model.evaluate(X_test,y_test,verbose=0)
-print(test_accuracy[1])
-
-# ---- USING THE MODEL TO PREDICT ----
-filename="fold8/103076-3-0-0.wav"
-audio, sample_rate = librosa.load(filename, res_type='kaiser_fast') 
-mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+    # load the labels
+    labels = pd.read_csv('./archive/UrbanSound8K.csv')
+    classes = np.array(labels['class'])
+    
+    # fit label encoder
+    labelencoder.fit(classes)
+    
+    # predict the class
+    predicted_label = model.predict(mfccs_scaled_features)
+    
+    classes_x = np.argmax(predicted_label, axis=1)
+    
+    # transform the predicted classes
+    prediction_class = labelencoder.inverse_transform(classes_x)
+    
+    return prediction_class
 
 
-mfccs_scaled_features=mfccs_scaled_features.reshape(1,-1)
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file uploaded'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No file selected'
+        try:
+            audio_data, sr = librosa.load(file, sr=None)
+        except Exception as e:
+            return f'Error processing file: {e}'
+        result = get_prediction(audio_data, sr)   
 
-predicted_label=model.predict(mfccs_scaled_features)
+        return render_template('result.html', RESULT=result)
+    return render_template('upload.html')
 
-classes_x=np.argmax(predicted_label,axis=1)
-prediction_class = labelencoder.inverse_transform(classes_x)
-print(prediction_class)
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
